913ed45e70d94c8cae2f423b094c2260c0543c4b
Abhishek <abhishekvas30@gmail.com>
2019-11-05 13:51:20 +0530

    try connect

--- connect.php
+++ connect.php
@@ -1,19 +1,14 @@
 <?php
+$dbhost = 'localhost';
+$username = 'root';
+$password = '';
+$db = 'home decor';
+// Create connection
+$conn = mysqli_connect("$dbhost" , "$username" , "$password");
+//echo "Connected" ;
+mysqli_select_db($conn,$db);
   $email = $_POST['email'];
   $pass = $_POST['password'];
-  $phn = $_POST['phone'];
-  $add = $_POST['address'];
-
-  //connection
-  $conn = new mysqli('localhost','root','','home decor');
-  if(@conn->connect_error){
-  	die('Connection Failed :'.$conn->connect_error);
-  }else
-  {
-  	$stmt=$conn->prepare("insert into data(Email,pass,phone,address) values(?,?,?,?)");
-  	$stmt->bind_param("ssis",$email,$pass,$phn,$add);
-  	$stmt->execute();
-  	$stmt->close();
-  	$conn->close();
-  }
-  ?>
+  $query = "insert into data(Email,pass,phone,address) values('$email','$pass','789','abc');";
+  $result = mysqli_query($conn,$query);
+?>
