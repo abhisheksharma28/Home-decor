defc29f33bc2b7b405405eb2772942553090b32a
Abhishek <abhishekvas30@gmail.com>
2019-11-07 22:53:37 +0530

    Create cart.php

---
+++ cart.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	  <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Dancing+Script" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+	<h1 class="text-center text-danger mb-5" 
+	style="font-family: 'Abril Fatface', cursive;">Home Decor Special Offers</h1>
+	<div class="row">
+		<div class="col-lg-3 col-md-3 col-sm-12">
+			<form>
+				<div class="card">
+					<h6 class="card-title bg-info text-white p-2 text-uppercase"> Light </h6>
+
+					<div class="card-body">
+						 <img src="light.jpg" alt="phone" class="img-fluid mb-2" >
+					 <h6> &#8377;<span> 1699/-</span></h6> 
+					 <h6 class="badge badge-success"> 4.4 <i class="fa fa-star"> </i> </h6>
+					 <input type="text" name="" class="form-control" placeholder="Quantity">
+					</div>
+					<div class="btn-group d-flex">
+						<button class="btn btn-success flex-fill"> Add to cart </button> <button class="btn btn-warning flex-fill text-white"> Buy Now </button>
+					</div>
+				</div>
+			</form>
+		</div>
+		<div class="col-lg-3 col-md-3 col-sm-12">
+			<form>
+				<div class="card">
+					<h6 class="card-title bg-info text-white p-2 text-uppercase"> Show piece </h6>
+
+					<div class="card-body">
+						 <img src="show.jpg" alt="phone" class="img-fluid mb-2" >
+					 <h6> &#8377;<span> 1599/-</span></h6> 
+					 <h6 class="badge badge-success"> 4.4 <i class="fa fa-star"> </i> </h6>
+					 <input type="text" name="" class="form-control" placeholder="Quantity">
+					</div>
+					<div class="btn-group d-flex">
+						<button class="btn btn-success flex-fill"> Add to cart </button> <button class="btn btn-warning flex-fill text-white"> Buy Now </button>
+					</div>
+				</div>
+			</form>
+		</div>
+		<div class="col-lg-3 col-md-3 col-sm-12">
+			<form>
+				<div class="card">
+					<h6 class="card-title bg-info text-white p-2 text-uppercase"> Furniture </h6>
+
+					<div class="card-body">
+						 <img src="light.jpg" alt="phone" class="img-fluid mb-2" >
+					 <h6> &#8377;<span>7999/-</span></h6> 
+					 <h6 class="badge badge-success"> 4.4 <i class="fa fa-star"> </i> </h6>
+					 <input type="text" name="" class="form-control" placeholder="Quantity">
+					</div>
+					<div class="btn-group d-flex">
+						<button class="btn btn-success flex-fill"> Add to cart </button> <button class="btn btn-warning flex-fill text-white"> Buy Now </button>
+					</div>
+				</div>
+			</form>
+		</div>
+		<div class="col-lg-3 col-md-3 col-sm-12">
+			<form>
+				<div class="card">
+					<h6 class="card-title bg-info text-white p-2 text-uppercase"> Decoration </h6>
+
+					<div class="card-body">
+						 <img src="decor.jpg" alt="phone" class="img-fluid mb-2" >
+					 <h6> &#8377;<span> 899/-</span></h6> 
+					 <h6 class="badge badge-success"> 4.4 <i class="fa fa-star"> </i> </h6>
+					 <input type="text" name="" class="form-control" placeholder="Quantity">
+					</div>
+					<div class="btn-group d-flex">
+						<button class="btn btn-success flex-fill"> Add to cart </button> <button class="btn btn-warning flex-fill text-white"> Buy Now </button>
+					</div>
+				</div>
+			</form>
+		</div>
+    </div>
+    <div class="row mt-4">
+		<div class="col-lg-3 col-md-3 col-sm-12">
+			<form>
+				<div class="card">
+					<h6 class="card-title bg-info text-white p-2 text-uppercase"> title </h6>
+
+					<div class="card-body">
+						 <img src="light.jpg" alt="phone" class="img-fluid mb-2" >
+					 <h6> &#8377;<span> rate</span></h6> 
+					 <h6 class="badge badge-success"> 4.4 <i class="fa fa-star"> </i> </h6>
+					 <input type="text" name="" class="form-control" placeholder="Quantity">
+					</div>
+					<div class="btn-group d-flex">
+						<button class="btn btn-success flex-fill"> Add to cart </button> <button class="btn btn-warning flex-fill text-white"> Buy Now </button>
+					</div>
+				</div>
+			</form>
+		</div>
+		<div class="col-lg-3 col-md-3 col-sm-12">
+			<form>
+				<div class="card">
+					<h6 class="card-title bg-info text-white p-2 text-uppercase"> title </h6>
+
+					<div class="card-body">
+						 <img src="light.jpg" alt="phone" class="img-fluid mb-2" >
+					 <h6> &#8377;<span> rate</span></h6> 
+					 <h6 class="badge badge-success"> 4.4 <i class="fa fa-star"> </i> </h6>
+					 <input type="text" name="" class="form-control" placeholder="Quantity">
+					</div>
+					<div class="btn-group d-flex">
+						<button class="btn btn-success flex-fill"> Add to cart </button> <button class="btn btn-warning flex-fill text-white"> Buy Now </button>
+					</div>
+				</div>
+			</form>
+		</div>
+		<div class="col-lg-3 col-md-3 col-sm-12">
+			<form>
+				<div class="card">
+					<h6 class="card-title bg-info text-white p-2 text-uppercase"> title </h6>
+
+					<div class="card-body">
+						 <img src="light.jpg" alt="phone" class="img-fluid mb-2" >
+					 <h6> &#8377;<span> rate</span></h6> 
+					 <h6 class="badge badge-success"> 4.4 <i class="fa fa-star"> </i> </h6>
+					 <input type="text" name="" class="form-control" placeholder="Quantity">
+					</div>
+					<div class="btn-group d-flex">
+						<button class="btn btn-success flex-fill"> Add to cart </button> <button class="btn btn-warning flex-fill text-white"> Buy Now </button>
+					</div>
+				</div>
+			</form>
+		</div>
+		<div class="col-lg-3 col-md-3 col-sm-12">
+			<form>
+				<div class="card">
+					<h6 class="card-title bg-info text-white p-2 text-uppercase"> title </h6>
+
+					<div class="card-body">
+						 <img src="light.jpg" alt="phone" class="img-fluid mb-2" >
+					 <h6> &#8377;<span> rate</span></h6> 
+					 <h6 class="badge badge-success"> 4.4 <i class="fa fa-star"> </i> </h6>
+					 <input type="text" name="" class="form-control" placeholder="Quantity">
+					</div>
+					<div class="btn-group d-flex">
+						<button class="btn btn-success flex-fill"> Add to cart </button> <button class="btn btn-warning flex-fill text-white"> Buy Now </button>
+					</div>
+				</div>
+			</form>
+		</div>
+    </div>
+    <div class="row mt-4">
+		<div class="col-lg-3 col-md-3 col-sm-12">
+			<form>
+				<div class="card">
+					<h6 class="card-title bg-info text-white p-2 text-uppercase"> title </h6>
+
+					<div class="card-body">
+						 <img src="light.jpg" alt="phone" class="img-fluid mb-2" >
+					 <h6> &#8377;<span> rate</span></h6> 
+					 <h6 class="badge badge-success"> 4.4 <i class="fa fa-star"> </i> </h6>
+					 <input type="text" name="" class="form-control" placeholder="Quantity">
+					</div>
+					<div class="btn-group d-flex">
+						<button class="btn btn-success flex-fill"> Add to cart </button> <button class="btn btn-warning flex-fill text-white"> Buy Now </button>
+					</div>
+				</div>
+			</form>
+		</div>
+		<div class="col-lg-3 col-md-3 col-sm-12">
+			<form>
+				<div class="card">
+					<h6 class="card-title bg-info text-white p-2 text-uppercase"> title </h6>
+
+					<div class="card-body">
+						 <img src="light.jpg" alt="phone" class="img-fluid mb-2" >
+					 <h6> &#8377;<span> rate</span></h6> 
+					 <h6 class="badge badge-success"> 4.4 <i class="fa fa-star"> </i> </h6>
+					 <input type="text" name="" class="form-control" placeholder="Quantity">
+					</div>
+					<div class="btn-group d-flex">
+						<button class="btn btn-success flex-fill"> Add to cart </button> <button class="btn btn-warning flex-fill text-white"> Buy Now </button>
+					</div>
+				</div>
+			</form>
+		</div>
+		<div class="col-lg-3 col-md-3 col-sm-12">
+			<form>
+				<div class="card">
+					<h6 class="card-title bg-info text-white p-2 text-uppercase"> title </h6>
+
+					<div class="card-body">
+						 <img src="light.jpg" alt="phone" class="img-fluid mb-2" >
+					 <h6> &#8377;<span> rate</span></h6> 
+					 <h6 class="badge badge-success"> 4.4 <i class="fa fa-star"> </i> </h6>
+					 <input type="text" name="" class="form-control" placeholder="Quantity">
+					</div>
+					<div class="btn-group d-flex">
+						<button class="btn btn-success flex-fill"> Add to cart </button> <button class="btn btn-warning flex-fill text-white"> Buy Now </button>
+					</div>
+				</div>
+			</form>
+		</div>
+		<div class="col-lg-3 col-md-3 col-sm-12">
+			<form>
+				<div class="card">
+					<h6 class="card-title bg-info text-white p-2 text-uppercase"> title </h6>
+
+					<div class="card-body">
+						 <img src="light.jpg" alt="phone" class="img-fluid mb-2" >
+					 <h6> &#8377;<span> rate</span></h6> 
+					 <h6 class="badge badge-success"> 4.4 <i class="fa fa-star"> </i> </h6>
+					 <input type="text" name="" class="form-control" placeholder="Quantity">
+					</div>
+					<div class="btn-group d-flex">
+						<button class="btn btn-success flex-fill"> Add to cart </button> <button class="btn btn-warning flex-fill text-white"> Buy Now </button>
+					</div>
+				</div>
+			</form>
+		</div>
+    </div>
+    <div class="row mt-4">
+		<div class="col-lg-3 col-md-3 col-sm-12">
+			<form>
+				<div class="card">
+					<h6 class="card-title bg-info text-white p-2 text-uppercase"> title </h6>
+
+					<div class="card-body">
+						 <img src="light.jpg" alt="phone" class="img-fluid mb-2" >
+					 <h6> &#8377;<span> rate</span></h6> 
+					 <h6 class="badge badge-success"> 4.4 <i class="fa fa-star"> </i> </h6>
+					 <input type="text" name="" class="form-control" placeholder="Quantity">
+					</div>
+					<div class="btn-group d-flex">
+						<button class="btn btn-success flex-fill"> Add to cart </button> <button class="btn btn-warning flex-fill text-white"> Buy Now </button>
+					</div>
+				</div>
+			</form>
+		</div>
+		<div class="col-lg-3 col-md-3 col-sm-12">
+			<form>
+				<div class="card">
+					<h6 class="card-title bg-info text-white p-2 text-uppercase"> title </h6>
+
+					<div class="card-body">
+						 <img src="light.jpg" alt="phone" class="img-fluid mb-2" >
+					 <h6> &#8377;<span> rate</span></h6> 
+					 <h6 class="badge badge-success"> 4.4 <i class="fa fa-star"> </i> </h6>
+					 <input type="text" name="" class="form-control" placeholder="Quantity">
+					</div>
+					<div class="btn-group d-flex">
+						<button class="btn btn-success flex-fill"> Add to cart </button> <button class="btn btn-warning flex-fill text-white"> Buy Now </button>
+					</div>
+				</div>
+			</form>
+		</div>
+		<div class="col-lg-3 col-md-3 col-sm-12">
+			<form>
+				<div class="card">
+					<h6 class="card-title bg-info text-white p-2 text-uppercase"> title </h6>
+
+					<div class="card-body">
+						 <img src="light.jpg" alt="phone" class="img-fluid mb-2" >
+					 <h6> &#8377;<span> rate</span></h6> 
+					 <h6 class="badge badge-success"> 4.4 <i class="fa fa-star"> </i> </h6>
+					 <input type="text" name="" class="form-control" placeholder="Quantity">
+					</div>
+					<div class="btn-group d-flex">
+						<button class="btn btn-success flex-fill"> Add to cart </button> <button class="btn btn-warning flex-fill text-white"> Buy Now </button>
+					</div>
+				</div>
+			</form>
+		</div>
+		<div class="col-lg-3 col-md-3 col-sm-12">
+			<form>
+				<div class="card">
+					<h6 class="card-title bg-info text-white p-2 text-uppercase"> title </h6>
+
+					<div class="card-body">
+						 <img src="light.jpg" alt="phone" class="img-fluid mb-2" >
+					 <h6> &#8377;<span> rate</span></h6> 
+					 <h6 class="badge badge-success"> 4.4 <i class="fa fa-star"> </i> </h6>
+					 <input type="text" name="" class="form-control" placeholder="Quantity">
+					</div>
+					<div class="btn-group d-flex">
+						<button class="btn btn-success flex-fill"> Add to cart </button> <button class="btn btn-warning flex-fill text-white"> Buy Now </button>
+					</div>
+				</div>
+			</form>
+		</div>
+    </div>
+    </div>
+</body>
+</html>
